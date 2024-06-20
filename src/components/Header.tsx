f3252bb2ee79e692b55e5585bc297b841b06ef39
Rain <pyc0422@gmail.com>
2024-06-20 14:49:04 -0500

    responsive header done

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,10 +6,12 @@ import { useAppDispatch } from "@/store/store";
 import { toggleModal } from "@/store/modalSlice";
 import ThemeToggle from "./ToggleTheme";
 import Link from "next/link";
+import { useState } from "react";
 const Header:React.FC = () => {
   const pathname = usePathname();
   const router = useRouter();
   const dispatch = useAppDispatch();
+  const [memuOpen, toggleMenu] = useState<boolean>(false)
   const handleLogOut = () => {
     document.cookie=`userId=;`;
     dispatch(toggleModal(false));
@@ -19,20 +21,35 @@ const Header:React.FC = () => {
     <nav className="flex justify-between gap-5 h-16 px-16 w-screen content-center items-center border-bottom shadow-lg">
       <Link href={'/'}><Image src={FullLogo} alt="Learn well logo" className="w-auto p-2" /></Link>
       <div className="flex gap-5 items-center">
-      {pathname !=='/login' && pathname !== '/'
-        && (
-        <div className="flex gap-5">
-          <button
-           onClick ={() => dispatch(toggleModal(true))}
-           className="bg-yellow px-2 py-1 md:w-36 text-white"
-          >Add Video</button>
-          <button
-           onClick={handleLogOut}
-           className="bg-green px-2 py-1 md:w-36 text-white"
-          >LogOut</button>
-        </div>
-      )}
+       <button
+          onClick={() => toggleMenu(!memuOpen)}
+          className="md:hidden p-2 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-500"
+        >
+          <svg className="w-6 h-6 text-black" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M4 6h16M4 12h16m-7 6h7"
+            />
+          </svg>
+        </button>
+        <div className={`md:flex gap-5 ${memuOpen ? 'absolute top-16 left-0 bg-white shadow-md bg-opacity-90 px-16 py-4' : 'hidden'} w-full md:w-auto items-center`}>
 
+          {pathname !=='/login' && pathname !== '/'
+            && (
+            <div className="flex flex-col md:flex-row md:items-center gap-5">
+              <button
+              onClick ={() => {toggleMenu(false); dispatch(toggleModal(true))}}
+              className="bg-yellow px-2 py-1 md:w-36 text-white"
+              >Add Video</button>
+              <button
+              onClick={handleLogOut}
+              className="bg-green px-2 py-1 md:w-36 text-white"
+              >LogOut</button>
+            </div>
+          )}
+        </div>
       <ThemeToggle />
       </div>
     </nav>
